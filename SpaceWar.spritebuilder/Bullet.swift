f73c04eb54267cc3233b28d072fa54ef717c0d0f
PlanetHorizont <steventianyizhang@163.com>
2015-09-05 20:48:41 -0700

    added Bullet class to unify all the bullet common methods

--- SpaceWar.spritebuilder/Bullet.swift
+++ SpaceWar.spritebuilder/Bullet.swift
@@ -1,8 +1,8 @@
 //
-//  Bullet.swift
+//  bullets.swift
 //  SpaceWar
 //
-//  Created by Tianyi Zhang on 30/08/2015.
+//  Created by Alcatraz on 2015-09-05.
 //  Copyright (c) 2015 Apportable. All rights reserved.
 //
 
@@ -10,20 +10,7 @@ import Foundation
 
 class Bullet: CCSprite {
     
-    func didLoadFromCCB() {
-        
-        var moveAnimation = CCActionMoveBy(duration: 0.5, position: CGPoint(x: 0, y: 200))
-        
-        var actionAfterAnimation = CCActionCallFunc (target: self, selector: "removeBullet")
-        
-        var moveSeq = CCActionSequence(one: moveAnimation, two: actionAfterAnimation)
-        
-        self.runAction(moveSeq)
-        
-    }
-    
     func removeBullet(){
         self.removeFromParent()
     }
-    
 }
